9522442a04190802e535f0dc303b1f6537395f32
Caleb Dudley <caleb.dudley@gmail.com>
2024-08-15 22:10:22 -0400

    Adding sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react(), sentryVitePlugin({
+    org: "space-dashboard",
+    project: "space-dashboard-js"
+  })],
   resolve: {
     alias: {
       src: "/src",
     },
   },
   build: {
-    sourcemap: process.env.NODE_ENV === 'development',
+    sourcemap: true,
     cssTarget: ['edge112', 'firefox117', 'chrome112', 'safari17'],
     cssMinify: true,
   },
